e55950a70fbd8e8a99ed80c0dbac192909fd13c9
mofei952 <695741200@qq.com>
2019-07-16 18:00:30 +0800

    add c2p16

---
+++ c02_strings_and_text/p16_reformat_text_to_fixed_number_columns.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/7/15 19:38
+# @File    : p16_reformat_text_to_fixed_number_columns.py
+# @Software: PyCharm
+
+# 以指定列宽格式化字符串
+# https://python3-cookbook.readthedocs.io/zh_CN/latest/c02/p16_reformat_text_to_fixed_number_columns.html
+
+# 使用 textwrap 模块来格式化字符串的输出
+s = "Look into my eyes, look into my eyes, the eyes, the eyes, \
+the eyes, not around the eyes, don't look around the eyes, \
+look into my eyes, you're under."
+import textwrap
+print(textwrap.fill(s, 70))
+print(textwrap.fill(s, 40))
+print(textwrap.fill(s, 40, initial_indent='    '))
+print(textwrap.fill(s, 40, subsequent_indent='    '))
+
+# 如果希望输出自动匹配终端大小，可以使用os.get_terminal_size()获取获取终端的大小尺寸
+# import os
+# print(os.get_terminal_size().columns)
