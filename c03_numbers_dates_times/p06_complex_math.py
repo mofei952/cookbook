9bd64f2bfd3b40f6fe164012e71078db5839ecb7
mofei952 <695741200@qq.com>
2019-07-25 21:29:39 +0800

    add c3p6

---
+++ c03_numbers_dates_times/p06_complex_math.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/7/25 21:18
+# @File    : p06_complex_math.py
+# @Software: PyCharm
+
+# 复数的数学运算
+# https://python3-cookbook.readthedocs.io/zh_CN/latest/c03/p06_complex_math.html
+
+# 使用复数的两种方式
+a = complex(2, 4)
+print(a)
+b = 3 - 5j
+print(b)
+
+# 取得对应的实部、虚部和共轭复数
+print(a.real)
+print(a.imag)
+print(a.conjugate())
+
+# 进行常见的数学运算
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(abs(a))
+
+# 进行正弦、余弦或平方根运算
+import cmath
+print(cmath.sin(a))
+print(cmath.cos(a))
+print(cmath.exp(a))
+
+# Python的标准数学函数并不能产生复数值，要使用cmath才可以
+# import math
+# print(math.sqrt(-1))  # ValueError: math domain error
+print(cmath.sqrt(-1))
