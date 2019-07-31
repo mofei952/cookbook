6268fe82274f141518cca91a57f19062e6a5ad2a
mofei952 <695741200@qq.com>
2019-07-31 21:27:15 +0800

    add c3p10

---
+++ c03_numbers_dates_times/p10_matrix_and_linear_algebra_calculation.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/7/27 18:37
+# @File    : p10_matrix_and_linear_algebra_calculation.py
+# @Software: PyCharm
+
+# 矩阵与线性代数运算
+# https://python3-cookbook.readthedocs.io/zh_CN/latest/c03/p10_matrix_and_linear_algebra_calculation.html
+
+# 使用NumPy的矩阵对象
+import numpy as np
+m = np.matrix([[1, -2, 3], [0, 4, 5], [7, 8, -9]])
+print(m)
+
+# 转置矩阵
+print(m.T)
+
+# 逆矩阵
+print(m.I)
+
+# 矩阵相乘
+v = np.matrix([[2], [3], [4]])
+print(v)
+print(m * v)
+
+# numpy.linalg 子包中有更多的操作函数
+# 矩阵的行列式
+print(np.linalg.det(m))
+
+# 矩阵的特征值
+print(np.linalg.eigvals(m))
+
+# mx = v方程中x的解
+x = np.linalg.solve(m, v)
+print(x)
+print(m * x)
+print(v)
