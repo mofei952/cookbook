31caeb6aacc5326db683f935b6c0a3dd3003a414
mofei952 <695741200@qq.com>
2019-08-03 20:10:02 +0800

    add c3p14

---
+++ c03_numbers_dates_times/p14_date_range_for_current_month.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/8/3 19:47
+# @File    : p14_date_range_for_current_month.py
+# @Software: PyCharm
+
+# 计算当前月份的日期范围
+# https://python3-cookbook.readthedocs.io/zh_CN/latest/c03/p14_date_range_for_current_month.html
+import calendar
+from datetime import date, timedelta, datetime
+
+# 先计算出开始日期和结束日期
+def get_month_range(start_date=None):
+    if start_date is None:
+        start_date = date.today()
+    _, days = calendar.monthrange(start_date.year, start_date.month)
+    start_date = start_date.replace(day=1)
+    end_date = start_date + timedelta(days=days)
+    return start_date, end_date
+
+# 使用 datetime.timedelta 递增日期变量
+start_date, end_date = get_month_range()
+a_day = timedelta(days=1)
+temp_date = start_date
+while temp_date < end_date:
+    print(temp_date)
+    temp_date += a_day
+
+# 使用生成器实现简单的日期迭代，类似于内置的range函数
+def date_range(start, end, step):
+    while start < end:
+        yield start
+        start += step
+for date_ in date_range(datetime(2019, 8, 1), datetime(2019, 8, 5), timedelta(hours=6)):
+    print(date_)
