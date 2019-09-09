e654f33c90d216fd8127ddfb159519d4d8663908
mofei952 <695741200@qq.com>
2019-09-09 21:49:19 +0800

    add c8p7

--- c08_classes_and_objects/p07_calling_method_on_parent_class.py
+++ c08_classes_and_objects/p07_calling_method_on_parent_class.py
@@ -31,7 +31,8 @@ class B(A):
 
 
 # super() 的另外一个常见用法出现在覆盖Python特殊方法的代码中
-class Proxy(object):
+# 就算没有显式的指明某个类的父类， super() 仍然可以有效的工作
+class Proxy:
     def __init__(self, obj):
         self._obj = obj
 
@@ -45,3 +46,56 @@ class Proxy(object):
             super().__setattr__(name, value)  # Call original __setattr__
         else:
             setattr(self._obj, name, value)
+
+
+# 每个类都有一个方法解析顺序(MRO)列表
+# 而这个MRO列表的构造是通过一个C3线性化算法来实现的
+# Python会在MRO列表上从左到右开始查找基类
+class Base:
+    def __init__():
+        print('Base.__init__')
+
+
+class A(Base):
+    def __init__():
+        super().__init__()
+        print('A.__init__')
+
+
+class B(Base):
+    def __init__():
+        super().__init__()
+        print('B.__init__')
+
+
+class C(A, B):
+    def __init__():
+        super().__init__()
+        print('C.__init__')
+
+
+print(C.mro())
+
+
+# super() 并不一定去查找某个类在MRO中下一个直接父类
+# 在类A中使用 super().spam() 实际上调用的是跟类A毫无关系的类B中的 spam() 方法
+# 由于 super() 可能会调用不是你想要的方法, 所以应该确保在继承体系中所有相同名字的方法拥有可兼容的参数签名(比如相同的参数个数和参数名称)。
+class A:
+    def spam(self):
+        print('A.spam')
+        super().spam()
+# a = A()
+# a.spam()#'super' object has no attribute 'spam'
+
+
+class B:
+    def spam(self):
+        print('B.spam')
+
+
+class C(A, B):
+    pass
+
+
+c = C()
+c.spam()
