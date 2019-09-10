2f12adc92bec0e69cc9af64a3d92767d67930c68
mofei952 <695741200@qq.com>
2019-09-10 21:10:12 +0800

    add c8p9 descriptor

---
+++ c08_classes_and_objects/p09_create_new_kind_of_class_or_instance_attribute.py
@@ -0,0 +1,78 @@
+# 创建新的类或实例属性
+# https://python3-cookbook.readthedocs.io/zh_CN/latest/c08/p09_create_new_kind_of_class_or_instance_attribute.html
+
+
+# 可以通过一个描述器类的形式来创建一个新的实例属性
+class Integer:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected an int')
+        instance.__dict__[self.name] = value
+
+    def __delete__(self, instance):
+        del instance.__dict__[self.name]
+
+
+class Point:
+    x = Integer('x')
+    y = Integer('y')
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+p = Point(1, 2)
+p.x = 3
+print(p.x)  # Calls Point.x.__get__(p, Point)
+print(Point.x)  # Calls Point.x.__get__(None, Point)
+
+
+# 类型检查属性的描述器，涉及到一个类装饰器
+class Typed:
+    def __init__(self, name, expected_type):
+        self.name = name
+        self.expected_type = expected_type
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self.expected_type):
+            raise TypeError('Expected ' + str(self.expected_type))
+        instance.__dict__[self.name] = value
+
+    def __delete__(self, instance):
+        del instance.__dict__[self.name]
+
+
+def typeassert(**kwargs):
+    def decorate(cls):
+        for name, expected_type in kwargs.items():
+            setattr(cls, name, Typed(name, expected_type))
+        return cls
+    return decorate
+
+
+@typeassert(name=str, shares=int, price=float)
+class Stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+
+s = Stock('1', 2, 3.1)
+# s.name = 1 # Expected <class 'str'>
