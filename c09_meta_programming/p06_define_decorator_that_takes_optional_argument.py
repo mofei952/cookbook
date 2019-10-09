9135cfe5840027770e78afb9af4eded40ca55d1f
mofei952 <695741200@qq.com>
2019-10-09 20:02:25 +0800

    add c9p6 decorator with optional argument

---
+++ c09_meta_programming/p06_define_decorator_that_takes_optional_argument.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/10/9 19:26
+# @File    : p06_define_decorator_that_takes_optional_argument.py
+# @Software: PyCharm
+
+# 带可选参数的装饰器
+# https://python3-cookbook.readthedocs.io/zh_CN/latest/c09/p06_define_decorator_that_takes_optional_argument.html
+
+import logging
+from functools import partial, wraps
+
+
+# 要实现一个装饰器既可以不传参数，比如@decorator
+# 又可以传递可选参数，比如@decorator(x, y, z)
+def logged(func=None, *, level=logging.DEBUG, name=None, message=None):
+    if func is None:
+        return partial(logged, level=level, name=name, message=message)
+
+    logname = name if name else func.__module__
+    log = logging.getLogger(logname)
+    logmsg = message if message else func.__name__
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        log.log(level, logmsg)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@logged
+def foo():
+    print('foo')
+
+
+@logged(level=logging.CRITICAL, name='test')
+def spam():
+    print('spam')
+
+
+logging.basicConfig(level=logging.DEBUG)
+foo()
+spam()
