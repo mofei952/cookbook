8d91d67522f28044b78c13a9fd17c37defa3fa43
mofei952 <695741200@qq.com>
2019-10-29 21:19:50 +0800

    add 9.12

---
+++ c09_meta_programming/p12_using_decorators_to_patch_class_definitions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/10/29 21:11
+# @File    : p12_using_decorators_to_patch_class_definitions.py
+# @Software: PyCharm
+
+# 使用装饰器扩充类的功能
+
+# 使用类装饰器打印日志
+def log_getattribute(cls):
+    orig_getattribute = cls.__getattribute__
+
+    def new_getattribute(self, name):
+        print('getting:', name)
+        return orig_getattribute(self, name)
+
+    cls.__getattribute__ = new_getattribute
+    return cls
+
+
+@log_getattribute
+class A:
+    def __init__(self, x):
+        self.x = x
+
+    def spam(self):
+        pass
+
+
+a = A(1)
+print(a.x)
+a.spam()
