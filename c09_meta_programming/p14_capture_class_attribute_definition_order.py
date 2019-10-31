9a2777c1a4c75620be4787b9825a50d9989ca242
mofei952 <695741200@qq.com>
2019-10-31 21:08:24 +0800

    add 9.14

---
+++ c09_meta_programming/p14_capture_class_attribute_definition_order.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/10/31 15:48
+# @File    : p14_capture_class_attribute_definition_order.py
+# @Software: PyCharm
+
+# 捕获类的属性定义顺序
+
+from collections import OrderedDict
+
+
+# 利用元类捕获类的定义信息
+class Typed:
+    _expected_type = type(None)
+
+    def __init__(self, name=None):
+        self._name = name
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self._expected_type):
+            raise TypeError('Expected' + str(self._expected_type))
+        instance.__dict__[self._name] = value
+
+
+class Integer(Typed):
+    _expected_type = int
+
+
+class Float(Typed):
+    _expected_type = float
+
+
+class String(Typed):
+    _expected_type = str
+
+
+class OrderedMeta(type):
+    def __new__(cls, clsname, bases, clsdict):
+        d = dict(clsdict)
+        order = []
+        for name, value in clsdict.items():
+            if isinstance(value, Typed):
+                value._name = name
+                order.append(name)
+        d['_order'] = order
+        return type.__new__(cls, clsname, bases, d)
+
+    @classmethod
+    def __prepare__(cls, clsname, bases):
+        return OrderedDict()
+
+
+class Structure(metaclass=OrderedMeta):
+    def as_csv(self):
+        return ','.join(str(getattr(self, name)) for name in self._order)
+
+
+class Stock(Structure):
+    name = String()
+    shares = Integer()
+    price = Float()
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+
+s = Stock('gg', 1, 1.2)
+print(s.name)
+print(s.as_csv())
+
+
+# 防止重复属性定义
+class NoDupOrderedDict(OrderedDict):
+    def __init__(self, clsname):
+        self.clsname = clsname
+        super().__init__()
+
+    def __setitem__(self, key, value):
+        if key in self:
+            raise ValueError('{} already defined in {}'.format(key, self.clsname))
+        super(NoDupOrderedDict, self).__setitem__(key, value)
+
+
+class OrderedMeta(type):
+    def __new__(cls, clsname, bases, clsdict):
+        d = dict(clsdict)
+        d['_order'] = [name for name in clsdict if name[0] != '_']
+        return type.__new__(cls, clsname, bases, d)
+
+    @classmethod
+    def __prepare__(mcs, clsname, bases):
+        return NoDupOrderedDict(clsname)
+
+
+class A(metaclass=OrderedMeta):
+    a = String()
+    b = Integer()
+    # a = Integer()  # ValueError: a already defined in A
+
+
+a = A()
+print(a._order)
