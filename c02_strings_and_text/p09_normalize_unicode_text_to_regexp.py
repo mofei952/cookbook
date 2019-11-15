be1ebeb6ac2aa3ce764a3b99907e1b65979cb63d
mofei952 <695741200@qq.com>
2019-11-15 21:15:05 +0800

    update 2.9-2.10

--- c02_strings_and_text/p09_normalize_unicode_text_to_regexp.py
+++ c02_strings_and_text/p09_normalize_unicode_text_to_regexp.py
@@ -6,20 +6,21 @@
 # @File    : p09_normalize_unicode_text_to_regexp.py
 # @Software: PyCharm
 
-# 将Unicode文本标准化
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c02/p09_normalize_unicode_text_to_regexp.html
+"""将Unicode文本标准化"""
+
+import unicodedata
 
 # 这里的文本”Spicy Jalapeño”使用了两种形式来表示。
 # 第一种使用整体字符”ñ”(U+00F1)，第二种使用拉丁字母”n”后面跟一个”~”的组合字符(U+0303)。
 s1 = 'Spicy Jalape\u00f1o'
 s2 = 'Spicy Jalapen\u0303o'
-print(s1, s2)
+print(s1)
+print(s2)
 print(s1 == s2)
 print(len(s1), len(s2))
 
 # 使用unicodedata模块先将文本标准化
 # NFC表示字符应该是整体组成(比如可能的话就使用单一编码)，而NFD表示字符应该分解为多个组合字符表示
-import unicodedata
 t1 = unicodedata.normalize('NFC', s1)
 t2 = unicodedata.normalize('NFC', s2)
 print(t1 == t2)
@@ -30,7 +31,7 @@ print(t3 == t4)
 print(ascii(t3))
 
 # Python同样支持扩展的标准化形式NFKC和NFKD，它们在处理某些字符的时候增加了额外的兼容特性。比如：
-s = '\ufb01' # A single character
+s = '\ufb01'  # A single character
 print(s)
 print(unicodedata.normalize('NFD', s))
 # Notice how the combined letters are broken apart here
