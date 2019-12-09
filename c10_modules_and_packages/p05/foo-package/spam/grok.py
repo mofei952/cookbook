f4f97d085bd5be37bf50f90da9832e4d6388bb39
mofei952 <695741200@qq.com>
2019-12-09 22:56:48 +0800

    add 10.5

---
+++ c10_modules_and_packages/p05/foo-package/spam/grok.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/12/9 22:24
+# @File    : grok.py
+# @Software: PyCharm
---
+++ c10_modules_and_packages/p05_separate_directories_import_by_namespace.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/12/9 21:55
+# @File    : p05_separate_directories_import_by_namespace.py
+# @Software: PyCharm
+
+# 通过导入有相同子模块的目录到python模块路径，这样会创建特殊的包命名空间模块
+import sys
+sys.path.extend(['p05/foo-package', 'p05/bar-package'])
+
+# 两个不同的包目录将合并在一起，并能够正常工作
+from spam import blah
+from spam import grok
+print(blah)
+print(grok)
+
+# 只读的目录列表副本被存储在其__path__变量中
+import spam
+print(spam.__path__)
+
+# 一个包是否被作为一个包命名空间的主要方法是检查其__file__属性。如果没有，那包是个命名空间
+# print(spam.__file__)  # AttributeError: module 'spam' has no attribute '__file__'
