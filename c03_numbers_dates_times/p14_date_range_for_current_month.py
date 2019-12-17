d7d73df4ffcf1ccecc467dd8fb903661a7ed89ee
mofei952 <695741200@qq.com>
2019-12-17 21:37:22 +0800

    update 3.14

--- c03_numbers_dates_times/p14_date_range_for_current_month.py
+++ c03_numbers_dates_times/p14_date_range_for_current_month.py
@@ -6,10 +6,11 @@
 # @File    : p14_date_range_for_current_month.py
 # @Software: PyCharm
 
-# 计算当前月份的日期范围
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c03/p14_date_range_for_current_month.html
+"""计算当前月份的日期范围"""
+
 import calendar
 from datetime import date, timedelta, datetime
+
 
 # 先计算出开始日期和结束日期
 def get_month_range(start_date=None):
@@ -20,6 +21,7 @@ def get_month_range(start_date=None):
     end_date = start_date + timedelta(days=days)
     return start_date, end_date
 
+
 # 使用 datetime.timedelta 递增日期变量
 start_date, end_date = get_month_range()
 a_day = timedelta(days=1)
@@ -28,10 +30,16 @@ while temp_date < end_date:
     print(temp_date)
     temp_date += a_day
 
+
 # 使用生成器实现简单的日期迭代，类似于内置的range函数
 def date_range(start, end, step):
     while start < end:
         yield start
         start += step
 for date_ in date_range(datetime(2019, 8, 1), datetime(2019, 8, 5), timedelta(hours=6)):
+    print(date_)
+
+
+# 结合以上两个内容
+for date_ in date_range(*get_month_range(), timedelta(days=3)):
     print(date_)
