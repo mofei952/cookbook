a2a2cb769470a7a0977bb3d356e1b9b26bc1391d
mofei952 <695741200@qq.com>
2019-12-19 21:46:05 +0800

    update 3.15

--- c03_numbers_dates_times/p15_convert_strings_into_datetimes.py
+++ c03_numbers_dates_times/p15_convert_strings_into_datetimes.py
@@ -6,11 +6,11 @@
 # @File    : p15_convert_strings_into_datetimes.py
 # @Software: PyCharm
 
-# 字符串转换为日期
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c03/p15_convert_strings_into_datetimes.html
+"""字符串转换为日期"""
+
+from datetime import datetime
 
 # 解析日期
-from datetime import datetime
 text = '2019-08-03'
 y = datetime.strptime(text, '%Y-%m-%d')
 z = datetime.now()
@@ -20,4 +20,11 @@ print(z - y)
 nice_z = datetime.strftime(z, '%A %B %d, %Y')
 print(nice_z)
 
+
 # strptime() 的性能比较差，若要解析大量的日期，可以自己实现一个解析函数
+def parse_ymd(s):
+    year_s, mon_s, day_s = s.split('-')
+    return datetime(int(year_s), int(mon_s), int(day_s))
+text = '2019-08-03'
+x = parse_ymd(text)
+print(x)
