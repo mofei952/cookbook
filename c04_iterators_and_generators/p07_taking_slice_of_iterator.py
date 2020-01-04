734bc31e38a2013011d06e824c2fd959842726ab
mofei952 <695741200@qq.com>
2020-01-04 21:57:06 +0800

    update 4.7 4.8

--- c04_iterators_and_generators/p07_taking_slice_of_iterator.py
+++ c04_iterators_and_generators/p07_taking_slice_of_iterator.py
@@ -6,21 +6,22 @@
 # @File    : p07_taking_slice_of_iterator.py
 # @Software: PyCharm
 
-# 迭代器切片
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c04/p07_taking_slice_of_iterator.html
+"""迭代器切片"""
 
-# 使用函数 itertools.islice() 在迭代器和生成器上做切片操作
+import itertools
+
+
+# 定义一个生成器
 def count(n):
     while True:
         yield n
         n += 1
+
+# 使用函数 itertools.islice() 在迭代器和生成器上做切片操作
 c = count(0)
 # print(c[10:20]) # TypeError: 'generator' object is not subscriptable
-
-import itertools
 for x in itertools.islice(count(0), 10, 20):
     print(x)
 
-# 函数 islice() 返回一个可以生成指定元素的迭代器
-# 它通过遍历并丢弃直到切片开始索引位置的所有元素。
-# 然后才开始一个个的返回元素，并直到切片结束索引位置
+# 函数 islice() 返回一个可以生成指定元素的迭代器，它通过遍历并丢弃直到切片开始索引位置的所有元素。
+# 然后才开始一个个的返回元素，并直到切片结束索引位置。
--- c04_iterators_and_generators/p08_skip_first_part_of_iterable.py
+++ c04_iterators_and_generators/p08_skip_first_part_of_iterable.py
@@ -6,30 +6,34 @@
 # @File    : p08_skip_first_part_of_iterable.py
 # @Software: PyCharm
 
-# 跳过可迭代对象的开始部分
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c04/p08_skip_first_part_of_iterable.html
+"""跳过可迭代对象的开始部分"""
 
-# 使用 itertools.dropwhile() 函数
-with open('p08_skip_first_part_of_iterable.txt') as f:
+from itertools import dropwhile
+from itertools import islice
+
+# 读取文件内容
+with open('p08.txt') as f:
     for line in f:
         print(line, end='')
 print()
-from itertools import dropwhile
-with open('p08_skip_first_part_of_iterable.txt') as f:
+
+# 使用 itertools.dropwhile() 函数来跳过文件开始的注释行
+with open('p08.txt') as f:
     for line in dropwhile(lambda line: line.startswith('#'), f):
         print(line, end='')
 print()
 
 # 已经知道了要跳过的元素的个数的话，那么可以使用 itertools.islice() 来代替
-from itertools import islice
 items = ['a', 'b', 'c', 1, 4, 10, 15]
 for x in islice(items, 3, None):
     print(x)
+print()
 for x in islice(items, None, 3):
     print(x)
+print()
 
 # 通常的过滤写法，会过滤所有符合条件的行，不只是开始部分
-with open('p08_skip_first_part_of_iterable.txt') as f:
+with open('p08.txt') as f:
     lines = (line for line in f if not line.startswith('#'))
     for line in lines:
         print(line, end='')
