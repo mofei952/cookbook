241f02790c662f962a806b10c0c95abc755b6a77
mofei952 <695741200@qq.com>
2020-01-05 17:05:55 +0800

    update 4.11

--- c04_iterators_and_generators/p11_iterate_over_multiple_sequences_simultaneously.py
+++ c04_iterators_and_generators/p11_iterate_over_multiple_sequences_simultaneously.py
@@ -6,35 +6,40 @@
 # @File    : p11_iterate_over_multiple_sequences_simultaneously.py
 # @Software: PyCharm
 
-# 同时迭代多个序列
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c04/p11_iterate_over_multiple_sequences_simultaneously.html
+"""同时迭代多个序列"""
+
+from itertools import zip_longest
 
 # 使用zip函数
 xs = [1, 2, 3, 4, 5]
 ys = [10, 20, 30, 40, 50]
 for x, y in zip(xs, ys):
     print(x, y)
+print()
 
 # zip函数的迭代长度跟参数中最短序列长度一致
 xs = [1, 2, 3, 4, 5]
 ys = [10, 20, 30]
 for x, y in zip(xs, ys):
     print(x, y)
+print()
 
 # 使用 itertools.zip_longest() 迭代长度和最长序列长度一致，没有值的位置补None
-from itertools import zip_longest
 for x, y in zip_longest(xs, ys):
     print(x, y)
+print()
 
 # 处理表头和数据
 headers = ['name', 'shares', 'price']
 values = ['ACME', 100, 490.1]
 print(dict(zip(headers, values)))
+print()
 
 # 处理表头和多行数据
 headers = ['name', 'shares', 'price']
 data = [['ACME', 100, 490.1], ['CC', 101, 422.1]]
 print([dict(zip(headers, line)) for line in data])
+print()
 
 # zip 可以接受多于两个序列的参数
 xs = [1, 2, 3, 4, 5]
