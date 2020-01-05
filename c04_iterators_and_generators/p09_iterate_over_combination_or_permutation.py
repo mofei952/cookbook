bc1df58c2fe024bb24ba68ffbcc6457a07b477a6
mofei952 <695741200@qq.com>
2020-01-05 17:02:00 +0800

    update 4.9 4.10

--- c04_iterators_and_generators/p09_iterate_over_combination_or_permutation.py
+++ c04_iterators_and_generators/p09_iterate_over_combination_or_permutation.py
@@ -6,29 +6,34 @@
 # @File    : p09_iterate_over_combination_or_permutation.py
 # @Software: PyCharm
 
-# 排列组合的迭代
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c04/p09_iterate_over_combination_or_permutation.html
+"""排列组合的迭代"""
+
+from itertools import combinations
+from itertools import combinations_with_replacement
+from itertools import permutations
 
 # 使用 itertools.permutations() 得到集合中所有元素的排列
-from itertools import permutations
 items = ['a', 'b', 'c', 'd']
 for p in permutations(items):
     print(p)
+print()
 for p in permutations(items, 2):
     print(p)
+print()
 
 # 使用 itertools.combinations 得到集合中所有元素的组合
-from itertools import combinations
 items = ['a', 'b', 'c']
 for c in combinations(items, 3):
     print(c)
+print()
 for c in combinations(items, 2):
     print(c)
+print()
 
 # 函数 itertools.combinations_with_replacement() 允许同一个元素被选择多次
-from itertools import combinations_with_replacement
 for c in combinations_with_replacement(items, 3):
     print(c)
+print()
 
 # 当碰到看上去有些复杂的迭代问题时，可以先看看itertools模块。
-# 如果这个问题很普遍，那么很有可能会在里面找到解决方案
+# 如果这个问题很普遍，那么很有可能会在里面找到解决方案。
--- c04_iterators_and_generators/p10_iterate_over_index_value_pairs_of_sequence.py
+++ c04_iterators_and_generators/p10_iterate_over_index_value_pairs_of_sequence.py
@@ -6,31 +6,41 @@
 # @File    : p10_iterate_over_index_value_pairs_of_sequence.py
 # @Software: PyCharm
 
-# 序列上索引值迭代
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c04/p10_iterate_over_index_value_pairs_of_sequence.html
+"""序列上索引值迭代"""
+
+from collections import defaultdict
 
 # 使用内置的enumerate函数
 items = ['a', 'b', 'c']
 for i, val in enumerate(items):
     print(i, val)
+print()
 
 # enumerate传递一个开始参数，代表行号从1开始
 for i, val in enumerate(items, 1):
     print(i, val)
+print()
 
 # 这种情况在遍历文件时想在错误消息中使用行号定位时候非常有用
-with open('p10_iterate_over_index_value_pairs_of_sequence.txt') as f:
+with open('p10.txt') as f:
     for lineno, line in enumerate(f):
         try:
             int(line)
         except Exception as e:
-            print(lineno, e)
+            print('Line {}: Parse error: {}'.format(lineno, e))
+print()
 
 # 将一个文件中出现的单词映射到它出现的行号上
-from collections import defaultdict
 word_summary = defaultdict(list)
-with open('p10_iterate_over_index_value_pairs_of_sequence2.txt') as f:
+with open('p10_2.txt') as f:
     for lineno, line in enumerate(f):
         for word in line.strip().lower().split():
             word_summary[word].append(lineno)
-print(word_summary)
+for word, linenos in word_summary.items():
+    print(word, linenos)
+print()
+
+# 在一个已经解压后的元组序列上使用 enumerate() 函数时容易出错，应该像下面这样写
+data = [(1, 2), (3, 4), (5, 6), (7, 8)]
+for n, (x, y) in enumerate(data):
+    print(n, x, y)
