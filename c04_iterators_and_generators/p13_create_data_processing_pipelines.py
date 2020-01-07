a6c595b90042349d503fc8d14217ca827b31cad2
mofei952 <695741200@qq.com>
2020-01-07 21:05:42 +0800

    update 4.13

--- c04_iterators_and_generators/p13_create_data_processing_pipelines.py
+++ c04_iterators_and_generators/p13_create_data_processing_pipelines.py
@@ -6,13 +6,14 @@
 # @File    : p13_create_data_processing_pipelines.py
 # @Software: PyCharm
 
-# 创建数据处理管道
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c04/p13_create_data_processing_pipelines.html
+"""创建数据处理管道"""
+
 import fnmatch
 import os
 import re
 
 
+# 假定要处理一个非常大的日志文件目录，可以定义一个由多个执行特定任务独立任务的简单生成器函数组成的容器
 def gen_find(filepat, top):
     for root, dirs, files in os.walk(top):
         for file in fnmatch.filter(files, filepat):
@@ -38,42 +39,47 @@ def gen_grep(pattern, lines):
             yield line
 
 
+# 将生成器函数连起来创建一个处理管道，比如查找包含单词python的所有日志行
 logfilenames = gen_find('log*', 'p13')
 logfiles = gen_opener(logfilenames)
 lines = gen_concatenate(logfiles)
 pylines = gen_grep('(?i)python', lines)
 for line in pylines:
     print(line)
+print()
 
-# 使用生成器表达式扩展通道
+# 之后可以再使用生成器表达式扩展通道，比如计算传输的字节数并计算其总和
 logfilenames = gen_find('log*', 'p13')
 logfiles = gen_opener(logfilenames)
 lines = gen_concatenate(logfiles)
 pylines = gen_grep('(?i)python', lines)
-countcolumn = (line.rsplit(None, 1)[1] for line in pylines)
-counts = (int(x) for x in countcolumn if x != '-')
-print(sum(counts))
+bytecolumn = (line.rsplit(None, 1)[1] for line in pylines)
+bytes = (int(x) for x in bytecolumn if x != '-')
+print(sum(bytes))
 print()
 
 
-# 自定义的search方法
+# 自定义search_file方法，可以在指定文件中查找包含单词python的所有行
 def search_file(filename, pattern):
+    pat = re.compile(pattern)
     with open(filename) as f:
         for line in f:
             line = line.strip()
-            if not re.search(pattern, line):
-                continue
-            yield line
+            if pat.search(line):
+                yield line
 
 
+for line in search_file('p13/log1.txt', '(?i)python'):
+    print(line)
+print()
+
+
+# search_files方法，可以查找目录中指定文件内容
 def search_files(top, filepat, pattern):
     filenames = gen_find(filepat, top)
     for filename in filenames:
         yield from search_file(filename, pattern)
 
 
-for line in search_file('p13/log1.txt', '(?i)python'):
-    print(line)
-print()
 for line in search_files('p13', 'log*', '(?i)python'):
     print(line)
