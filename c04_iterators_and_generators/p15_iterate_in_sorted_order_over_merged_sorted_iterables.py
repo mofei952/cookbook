a580fc6c802118d75ac60065e6d965aa4a3d6d0d
mofei952 <695741200@qq.com>
2020-01-09 20:37:12 +0800

    update 4.15 4.16

--- c04_iterators_and_generators/p15_iterate_in_sorted_order_over_merged_sorted_iterables.py
+++ c04_iterators_and_generators/p15_iterate_in_sorted_order_over_merged_sorted_iterables.py
@@ -6,18 +6,17 @@
 # @File    : p15_iterate_in_sorted_order_over_merged_sorted_iterables.py
 # @Software: PyCharm
 
-# 顺序迭代合并后的排序迭代对象
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c04/p15_iterate_in_sorted_order_over_merged_sorted_iterables.html
+"""顺序迭代合并后的排序迭代对象"""
 
 import heapq
 
 a = [1, 4, 7, 10]
 b = [2, 5, 6, 11]
-for i in heapq.merge(a, b):
-    print(i)
 
-# heapq.merge 可迭代特性意味着它不会立马读取所有序列。
-# 这就意味着可以在非常长的序列中使用，而不会有太大的开销
+for i in heapq.merge(a, b):
+    print(i, end=',')
+
+# heapq.merge 可迭代特性意味着它不会立马读取所有序列。这就意味着可以在非常长的序列中使用，而不会有太大的开销
 
 # heapq.merge() 需要所有输入序列必须是排过序的。
 # 它并不会预先读取所有数据到堆栈中或者预先排序，也不会对输入做任何的排序检测。
--- c04_iterators_and_generators/p16_replace_infinite_while_loops_with_iterator.py
+++ c04_iterators_and_generators/p16_replace_infinite_while_loops_with_iterator.py
@@ -6,21 +6,23 @@
 # @File    : p16_replace_infinite_while_loops_with_iterator.py
 # @Software: PyCharm
 
-# 迭代器代替while无限循环
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c04/p16_replace_infinite_while_loops_with_iterator.html
+"""迭代器代替while无限循环"""
 
 # 常见的IO操作
+import sys
+
 with open('p16.txt', 'r') as f:
     while True:
         data = f.read(10)
         if not data:
             break
-        print(data)
+        print(data, end='')
+print()
 
-# 使用iter代替
+# 使用iter来进行IO操作
 with open('p16.txt', 'r') as f:
     for line in iter(lambda: f.read(10), ''):
-        print(line)
+        sys.stdout.write(line)
 
 # iter 函数可以接受一个可选的 callable 对象和一个标记(结尾)值作为输入参数。
 # 当以这种方式使用的时候，它会创建一个迭代器， 这个迭代器会不断调用 callable 对象直到返回值和标记值相等为止
