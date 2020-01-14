9d32c22c3b2811e6271d567374bb8bb7f376a39a
mofei952 <695741200@qq.com>
2020-01-14 20:52:00 +0800

    update 5.5 5.6

--- c05_files_and_io/p05_write_to_file_not_exist.py
+++ c05_files_and_io/p05_write_to_file_not_exist.py
@@ -6,8 +6,9 @@
 # @File    : p05_write_to_file_not_exist.py
 # @Software: PyCharm
 
-# 文件不存在才能写入
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c05/p05_write_to_file_not_exist.html
+"""文件不存在才能写入"""
+
+import os
 
 # 在 open() 函数中使用 x 模式来代替 w 模式
 # 使用x模式是对写文件时通常会遇到的一个问题的完美解决方案（不小心覆盖了一个已存在的文件）
@@ -19,11 +20,8 @@ with open('p05.txt', 'wt') as f:
 # FileExistsError: [Errno 17] File exists: 'p05.txt'
 
 # 另一种解决方案时先测试这个文件是否存在
-import os
 if not os.path.exists('p05.txt'):
     with open('p05.txt', 'wt') as f:
         f.write('Hello\n')
 else:
     print('File already exists!')
-
-# 显而易见，使用x文件模式更加简单
--- c05_files_and_io/p06_io_operations_on_string.py
+++ c05_files_and_io/p06_io_operations_on_string.py
@@ -6,24 +6,24 @@
 # @File    : p06_io_operations_on_string.py
 # @Software: PyCharm
 
-# 字符串的I/O操作
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c05/p06_io_operations_on_string.html
+"""字符串的I/O操作"""
 
 import io
 
-# 使用操作类文件对象的程序来操作文本或二进制字符串
+# 使用 io.StringIO() 类来创建类文件对象操作字符串数据
 s = io.StringIO()
-s.write('hello ')
+s.write('hello\n')
 print('world', file=s)
-print(s.getvalue())
+print(repr(s.getvalue()))
 
+s = io.StringIO('Hello\nWorld\n')
+print(repr(s.read(4)))
+print(repr(s.read()))
+
+# io.StringIO 只能用于文本。如果要操作二进制数据，要使用 io.BytesIO 类来代替
 s = io.BytesIO()
 s.write(b'bin')
 print(s.getvalue())
 
 # 当想要模拟一个普通的文件的时候 StringIO 和 BytesIO 类是很有用的。
-# 比如，在单元测试中，可以使用 StringIO 来创建一个包含测试数据的类文件对象，
-# 这个对象可以被传给某个参数为普通文件对象的函数。
-#
-# 需要注意的是， StringIO 和 BytesIO 实例并没有正确的整数类型的文件描述符。
-# 因此，它们不能在那些需要使用真实的系统级文件如文件，管道或者是套接字的程序中使用。
+# 比如，在单元测试中，可以使用 StringIO 来创建一个包含测试数据的类文件对象， 这个对象可以被传给某个参数为普通文件对象的函数。
