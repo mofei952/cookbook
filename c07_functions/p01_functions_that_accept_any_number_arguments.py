3d45275c3bd8a9ea2c857d8b025ad6c858948b00
mofei952 <695741200@qq.com>
2020-03-29 14:03:14 +0800

    update 7.1

--- c07_functions/p01_functions_that_accept_any_number_arguments.py
+++ c07_functions/p01_functions_that_accept_any_number_arguments.py
@@ -6,35 +6,45 @@
 # @File    : p01_functions_that_accept_any_number_arguments.py
 # @Software: PyCharm
 
-# 可接受任意数量参数的函数
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c07/p01_functions_that_accept_any_number_arguments.html
+"""可接受任意数量参数的函数"""
+
+import html
+
 
 # 让一个函数接受任意数量的位置参数，可以使用*参数
 def avg(first, *rest):
     return (first + sum(rest)) / (1 + len(rest))
 
+
 print(avg(1.5, 2.5))
 print(avg(1, 2, 3, 4))
 
+
 # 让函数接受任意数量的关键字参数，可以使用**开头的参数
-import html
 def make_element(name, value, **attrs):
-    attr_str = ' '.join('{}="{}"'.format(k,v) for k,v in attrs.items())
+    attr_str = ' '.join('{}="{}"'.format(k, v) for k, v in attrs.items())
     element = '<{name} {attrs}>{value}</{name}>'.format(name=name, attrs=attr_str, value=(html.escape(value)))
     return element
+
 
 print(make_element('item', 'aaa', size='large', quantity=6))
 print(make_element('p', '<spam>'))
 
+
 # 同时接受任意数量的位置参数和关键字参数，可以同时使用*和**参数
 def anyargs(*args, **kwargs):
-    print(args) # A tuple
-    print(kwargs) # A dict
+    print(args)  # A tuple
+    print(kwargs)  # A dict
+
+
+anyargs(1, 2, 3, a=4, b=5)
+
 
 # 一个*参数只能出现在函数定义中最后一个位置参数后面，而**参数只能出现在最后一个参数。
 # 有一点要注意的是，在*参数后面仍然可以定义其他参数。这种参数就是所谓的强制关键字参数
 def a(x, *args, y):
     pass
+
 
 def b(x, *args, y, **kwargs):
     pass
