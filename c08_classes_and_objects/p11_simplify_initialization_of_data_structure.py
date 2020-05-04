893fc62df5934d6be115b44382263d186f45601a
mofei952 <695741200@qq.com>
2020-05-04 19:28:44 +0800

    update 8.11

--- c08_classes_and_objects/p11_simplify_initialization_of_data_structure.py
+++ c08_classes_and_objects/p11_simplify_initialization_of_data_structure.py
@@ -1,5 +1,12 @@
-# 简化数据结构的初始化
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c08/p11_simplify_initialization_of_data_structure.html
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2020/5/1 16:04
+# @File    : p11_simplify_initialization_of_data_structure.py
+# @Software: PyCharm
+
+"""简化数据结构的初始化"""
 
 import math
 
@@ -33,11 +40,11 @@ class Circle(Structure1):
 s = Stock('aa', 10, 30)
 p = Point(2, 3)
 c = Circle(4.5)
-print(s.name)
+print(s.name, s.shares, s.price)
 # s = Stock('1', '2') # TypeError: Expected 3 arguments
+print()
 
 
-# 支持关键字参数的方案
 class Structure2:
     _fields = []
 
@@ -45,15 +52,12 @@ class Structure2:
         if len(args) > len(self._fields):
             raise TypeError('Expected {} arguments'.format(len(self._fields)))
 
-        # Set all of the positional arguments
         for name, value in zip(self._fields, args):
             setattr(self, name, value)
 
-        # Set the remaining keyword arguments
         for name in self._fields[len(args):]:
             setattr(self, name, kwargs.pop(name))
 
-        # Check for any remaining unknown arguments
         if kwargs:
             raise TypeError('Invalid argument(s): {}'.format(','.join(kwargs)))
 
@@ -63,12 +67,12 @@ class Stock(Structure2):
 
 
 s1 = Stock('ACME', 50, 91.1)
-s2 = Stock('ACME', 50, price=91.1)
+s2 = Stock('ACME', 50, price=9.1)
 s3 = Stock('ACME', shares=50, price=91.1)
-# KeyError: 'price'
-# s4 = Stock('ACME', shares=50)
-# TypeError: Invalid argument(s): aa,bb
-# s5 = Stock('ACME', shares=50, price=91.1, aa=1, bb=2)
+# s4 = Stock('ACME', shares=50)  # KeyError: 'price'
+# s5 = Stock('ACME', shares=50, price=91.1, aa=1, bb=2)  # TypeError: Invalid argument(s): aa,bb
+print(s1.name, s1.shares, s1.price)
+print()
 
 
 # 也可以将不在 _fields 中的名称加入到属性中去
@@ -99,3 +103,4 @@ class Stock(Structure3):
 
 s1 = Stock('ACME', 50, 91.1)
 s2 = Stock('ACME', 50, 91.1, date='8/2/2012')
+print(s2.name, s2.shares, s2.price, s2.date)
