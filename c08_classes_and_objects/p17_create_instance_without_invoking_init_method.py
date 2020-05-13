a1596d42e301bd74db0209e65cf2c836fe79148b
mofei952 <695741200@qq.com>
2020-05-13 21:08:16 +0800

    update 8.17

--- c08_classes_and_objects/p17_create_instance_without_invoking_init_method.py
+++ c08_classes_and_objects/p17_create_instance_without_invoking_init_method.py
@@ -6,13 +6,13 @@
 # @File    : p17_create_instance_without_invoking_init_method.py
 # @Software: PyCharm
 
-# 创建不调用init方法的实例
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c08/p17_create_instance_without_invoking_init_method.html
+"""创建不调用init方法的实例"""
 
-# 通过__new__方法创建一个未初始化的实例
+import json
 from time import localtime
 
 
+# 通过__new__方法创建一个未初始化的实例
 class Date:
     def __init__(self, year, month, day):
         self.year = year
@@ -43,5 +43,22 @@ class NewDate(Date):
         d.day = t.tm_mday
         return d
 
+    def __repr__(self):
+        return '{} {:04d}-{:02d}-{:02d}'.format(self.__class__, self.year, self.month, self.day)
+
 
 print(NewDate.today())
+
+
+# 反序列化JSON数据时，可以使用__new__函数
+class NewNewDate(NewDate):
+    @classmethod
+    def from_json(cls, data):
+        d = cls.__new__(cls)
+        d.year = data['year']
+        d.month = data['month']
+        d.day = data['day']
+        return d
+
+
+print(json.loads('{"year": 2012, "month": 8, "day": 29}', object_hook=NewNewDate.from_json))
