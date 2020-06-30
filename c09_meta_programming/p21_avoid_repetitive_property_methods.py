5d5f2c460a8a822df377dcad40e637e7f24fae72
mofei952 <695741200@qq.com>
2020-06-30 22:09:57 +0800

    add 9.21

---
+++ c09_meta_programming/p21_avoid_repetitive_property_methods.py
@@ -0,0 +1,70 @@
+from functools import partial
+
+"""避免重复的属性方法"""
+
+
+# 使用property管理来属性可能会产生了一些重复代码
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('name must be a string')
+        self._name = value
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if not isinstance(value, int):
+            raise TypeError('age must be a int')
+        self._age = value
+
+
+# 可以创建一个函数来简化它
+def typed_property(name, expected_type):
+    storage_name = '_' + name
+
+    @property
+    def prop(self):
+        return getattr(self, storage_name)
+
+    @prop.setter
+    def prop(self, value):
+        if not isinstance(value, expected_type):
+            raise TypeError(f'{name} must be a {expected_type}')
+        setattr(self, storage_name, value)
+
+    return prop
+
+
+class Person:
+    name = typed_property('name', str)
+    age = typed_property('age', int)
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# 还可以使用 functools.partial() 来稍稍改变下这个例子，可以发现这里的用法和8.13小节的类型系统描述器有点相似
+String = partial(typed_property, expected_type=str)
+Integer = partial(typed_property, expected_type=int)
+
+
+class Person:
+    name = String('name')
+    age = Integer('age')
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
