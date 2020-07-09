0a9931360c7212944523018b8de188332b888afc
mofei952 <695741200@qq.com>
2020-07-09 21:09:35 +0800

    update 9.25

--- c09_meta_programming/p25_disassembling_python_byte_code.py
+++ c09_meta_programming/p25_disassembling_python_byte_code.py
@@ -2,6 +2,7 @@
 
 import dis
 import opcode
+import types
 
 
 # dis模块可以被用来输出任何Python函数的反编译结果
@@ -36,16 +37,39 @@ def generate_opcodes(codebytes):
         op = codebytes[i]
         i += 1
         if op >= opcode.HAVE_ARGUMENT:
-            oparg = codebytes[i] + codebytes[i+1] * 256 + extended_arg
+            oparg = codebytes[i] + extended_arg
             extended_arg = 0
-            i += 2
             if op == opcode.EXTENDED_ARG:
                 extended_arg = oparg * 65535
                 continue
         else:
             oparg = None
+        i += 1
         yield (op, oparg)
 
 
 for op, oparg in generate_opcodes(countdown.__code__.co_code):
-    print(op, opcode.opname[op], oparg)
+    if oparg is not None:
+        print('{:15} {:20} {:5}'.format(op, opcode.opname[op], oparg))
+    else:
+        print('{:15} {:20}'.format(op, opcode.opname[op]))
+print()
+
+
+# 替换任意函数的原始字节码
+def add(x, y):
+    return x + y
+
+
+c = add.__code__
+print(c)
+print(c.co_code)
+
+newbytecode = b'xxxxxxx'
+nc = types.CodeType(c.co_argcount, c.co_kwonlyargcount, c.co_nlocals,
+                    c.co_stacksize, c.co_flags, newbytecode, c.co_consts,
+                    c.co_names, c.co_varnames, c.co_filename, c.co_name,
+                    c.co_firstlineno, c.co_lnotab)
+print(nc)
+add.__code__ = nc
+print(add(2, 3))
