58921e4cbf82362e906e56c7180db8eb15a3fec2
mofei952 <695741200@qq.com>
2020-07-18 21:34:22 +0800

    update 10.2

--- c10_modules_and_packages/p02_control_the_import_of_everything.py
+++ c10_modules_and_packages/p02_control_the_import_of_everything.py
@@ -6,6 +6,8 @@
 # @File    : p02_control_the_import_of_everything.py
 # @Software: PyCharm
 
+""" 控制模块被全部导入的内容 """
+
 # from module import * 默认会导入所有不以下划线开头的
 # 如果定义了 __all__ , 那么只有被列举出的东西会被导入
 from c10_modules_and_packages.p02.test import *
