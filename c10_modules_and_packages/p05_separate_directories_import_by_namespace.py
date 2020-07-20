dbed00116c3ba3ee53eafdf0b43f8fec86da405e
mofei952 <695741200@qq.com>
2020-07-20 20:59:52 +0800

    update 10.5

--- c10_modules_and_packages/p05_separate_directories_import_by_namespace.py
+++ c10_modules_and_packages/p05_separate_directories_import_by_namespace.py
@@ -6,6 +6,9 @@
 # @File    : p05_separate_directories_import_by_namespace.py
 # @Software: PyCharm
 
+""" 利用命名空间导入目录分散的代码 """
+
+
 # 通过导入有相同子模块的目录到python模块路径，这样会创建特殊的包命名空间模块
 import sys
 sys.path.extend(['p05/foo-package', 'p05/bar-package'])
@@ -16,6 +19,7 @@ from spam import grok
 print(blah)
 print(grok)
 
+# 包命名空间的关键是确保顶级目录中没有__init__.py文件来作为共同的命名空间
 # 只读的目录列表副本被存储在其__path__变量中
 import spam
 print(spam.__path__)
