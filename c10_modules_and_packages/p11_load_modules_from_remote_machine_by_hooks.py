644902ca5073c34566e8b1e05d8dfc4c59870623
mofei952 <695741200@qq.com>
2020-08-01 20:55:50 +0800

    update 10.11

--- c10_modules_and_packages/p11_load_modules_from_remote_machine_by_hooks.py
+++ c10_modules_and_packages/p11_load_modules_from_remote_machine_by_hooks.py
@@ -1,11 +1,14 @@
 import imp
 from urllib.request import urlopen
 import sys
+import logging
+from html.parser import HTMLParser
+import importlib
 
 
 # 将一些文件作为模块被远程访问
-# cd p11
-# python -m http.sererver 15000
+# cd c10_modules_and_packages\p11
+# python -m http.server 15000
 
 
 # 加载远程模块
@@ -21,12 +24,163 @@ def load_module(url):
     return mod
 
 
-fib = load_module('http://localhost:15000/fib.py')
-print(fib)
-for i in range(10):
-    print(fib.fib(i), end=',')
-print()
+# fib = load_module('http://localhost:15000/fib.py')
+# print(fib)
+# for i in range(10):
+#     print(fib.fib(i), end=',')
+# print()
 
-spam = load_module('http://localhost:15000/spam.py')
-print(spam)
-spam.hello('Guido')
+# spam = load_module('http://localhost:15000/spam.py')
+# print(spam)
+# spam.hello('Guido')
+
+
+# 自定义导入器
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+
+def _get_links(url):
+    class LinkParser(HTMLParser):
+        def handle_starttag(self, tag, attrs):
+            if tag == 'a':
+                attrs = dict(attrs)
+                links.add(attrs.get('href').rstrip('/'))
+    links = set()
+    try:
+        log.debug('Getting links from %s', url)
+        u = urlopen(url)
+        parser = LinkParser()
+        parser.feed(u.read().decode('utf-8'))
+    except Exception as e:
+        log.debug('Could not get links. %s', e)
+    log.debug('links: %r', links)
+    return links
+
+
+class UrlMetaFinder(importlib.abc.MetaPathFinder):
+    def __init__(self, baseurl):
+        self._baseurl = baseurl
+        self._links = {}
+        self._loaders = {baseurl: UrlModuleLoader(baseurl)}
+
+    def find_module(self, fullname, path=None):
+        log.debug('find_module: fullname=%r, path=%r', fullname, path)
+        if path is None:
+            baseurl = self._baseurl
+        else:
+            if not path[0].startswith(self._baseurl):
+                return None
+            baseurl = path[0]
+        parts = fullname.split('.')
+        basename = parts[-1]
+        log.debug('find_module: baseurl=%r, basename=%r', baseurl, basename)
+
+        if baseurl not in self._links:
+            self._links[baseurl] = _get_links(baseurl)
+
+        if basename in self._links[baseurl]:
+            log.debug('find_module: trying package %r', fullname)
+            fullurl = self._baseurl + '/' + basename
+            # Attempt to load the package (which accesses __init__.py)
+            loader = UrlPackageLoader(fullurl)
+            try:
+                loader.load_module(fullname)
+                self._links[fullurl] = _get_links(fullurl)
+                self._loaders[fullurl] = UrlModuleLoader(fullurl)
+                log.debug('find_module: package %r loaded', fullname)
+            except Exception as e:
+                log.debug('find_module: package failed. %s', e)
+                loader = None
+            return loader
+
+        filename = basename + '.py'
+        if filename in self._links[baseurl]:
+            log.debug('find_module: module %r found', fullname)
+            return self._loaders[baseurl]
+        else:
+            log.debug('find_module: module %r not found', fullname)
+            return None
+
+
+class UrlModuleLoader(importlib.abc.SourceLoader):
+    def __init__(self, baseurl):
+        self._baseurl = baseurl
+        self._source_cache = {}
+
+    def module_repr(self, module):
+        return '<urlmodule %r from %r>' % (module.__name__, module.__file__)
+
+    def load_module(self, fullname):
+        code = self.get_code(fullname)
+        mod = sys.modules.setdefault(fullname, imp.new_module(fullname))
+        mod.__file__ = self.get_filename(fullname)
+        mod.__loader__ = self
+        mod.__package__ = fullname.rpartition('.')[0]
+        log.debug('test %s', mod.__name__)
+        exec(code, mod.__dict__)
+        return mod
+
+    def get_code(self, fullname):
+        src = self.get_source(fullname)
+        return compile(src, self.get_filename(fullname), 'exec')
+
+    def get_data(self, path):
+        pass
+
+    def get_filename(self, fullname):
+        return self._baseurl + '/' + fullname.split('.')[-1] + '.py'
+
+    def get_source(self, fullname):
+        filename = self.get_filename(fullname)
+        log.debug('loader: reading %r', filename)
+        if filename in self._source_cache:
+            log.debug('loader: cached %r', filename)
+            return self._source_cache[filename]
+        try:
+            u = urlopen(filename)
+            source = u.read().decode('utf-8')
+            log.debug('loader: %r loaded', filename)
+            self._source_cache[filename] = source
+            return source
+        except () as e:
+            log.debug('loader: %r failed. %s', filename, e)
+            raise ImportError("Can't load %s" % filename)
+
+
+class UrlPackageLoader(UrlModuleLoader):
+    def load_module(self, fullname):
+        mod = super().load_module(fullname)
+        mod.__path__ = [self._baseurl]
+        mod.__package__ = fullname
+
+    def get_filename(self, fullname):
+        return self._baseurl + '/' + '__init__.py'
+
+
+_installed_meta_cache = {}
+
+
+def install_meta(address):
+    if address not in _installed_meta_cache:
+        finder = UrlMetaFinder(address)
+        _installed_meta_cache[address] = finder
+        sys.meta_path.append(finder)
+        log.debug('%r installed on sys.meta_path', finder)
+
+
+def remove_meta(address):
+    if address in _installed_meta_cache:
+        finder = _installed_meta_cache[address]
+        sys.meta_path.remove(finder)
+        log.debug('%r removed from sys.meta_path', finder)
+
+
+install_meta('http://localhost:15000')
+import fib
+print(fib)
+print(fib.fib(1))
+import spam
+import grok.blah
+print(grok.blah)
+print(grok.blah.__file__)
