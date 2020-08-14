ce18e2c11b1efbfed7a57bde74435bbda64d5eaf
mofei952 <695741200@qq.com>
2020-08-14 21:02:01 +0800

    add 10.13

---
+++ c10_modules_and_packages/p13_installing_packages_just_for_yourself.py
@@ -0,0 +1,8 @@
+""" 安装私有的包 """
+
+# Python有一个用户安装目录，通常类似”~/.local/lib/python3.3/site-packages”。
+# 要强制在这个目录中安装包，可使用安装选项“–user”。
+"""
+python3 setup.py install --user
+pip install --user packagename
+"""
