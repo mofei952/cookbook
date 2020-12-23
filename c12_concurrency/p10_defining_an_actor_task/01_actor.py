4d6ab3be79e3a634002d8fd95cb5822ee55c7ab2
mofei952 <695741200@qq.com>
2020-12-23 22:39:03 +0800

    add: 12.10 defining an actor task

---
+++ c12_concurrency/p10_defining_an_actor_task/01_actor.py
@@ -0,0 +1,80 @@
+from queue import Queue
+from threading import Thread, Event
+
+
+# 结合使用一个线程和一个队列可以很容易的定义actor
+
+# Sentinel used for shutdown
+class ActorExit(Exception):
+    pass
+
+
+class Actor:
+    def __init__(self):
+        self._mailbox = Queue()
+
+    def send(self, msg):
+        '''
+        Send a message to the actor
+        '''
+        self._mailbox.put(msg)
+
+    def recv(self):
+        '''
+        Receive an incoming message
+        '''
+        msg = self._mailbox.get()
+        if msg is ActorExit:
+            raise ActorExit()
+        return msg
+
+    def close(self):
+        '''
+        Close the actor, thus shutting it down
+        '''
+        self.send(ActorExit)
+
+    def start(self):
+        '''
+        Start concurrent execution
+        '''
+        self._terminated = Event()
+        t = Thread(target=self._bootstrap)
+
+        t.daemon = True
+        t.start()
+
+    def _bootstrap(self):
+        try:
+            self.run()
+        except ActorExit:
+            pass
+        finally:
+            self._terminated.set()
+
+    def join(self):
+        self._terminated.wait()
+
+    def run(self):
+        '''
+        Run method to be implemented by the user
+        '''
+        while True:
+            msg = self.recv()
+
+
+# Sample ActorTask
+class PrintActor(Actor):
+    def run(self):
+        while True:
+            msg = self.recv()
+            print('Got:', msg)
+
+
+# Sample use
+p = PrintActor()
+p.start()
+p.send('Hello')
+p.send('World')
+p.close()
+p.join()
