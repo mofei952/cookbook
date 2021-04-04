52aa5dea2bfaeef3ec19e3bf2356ec85dea2faf2
mofei952 <695741200@qq.com>
2021-04-04 11:24:24 +0800

    upate 13.12 function library use independent log level

--- c13_utility_script_and_system_manage/p12_add_logging_to_libraries/main.py
+++ c13_utility_script_and_system_manage/p12_add_logging_to_libraries/main.py
@@ -2,5 +2,11 @@ import logging
 import somelib
 
 
-# logging.basicConfig()
+logging.basicConfig()
 somelib.func()
+print()
+
+logging.getLogger('somelib').level = logging.DEBUG
+somelib.func()
+logging.debug('111')
+logging.warning('111')
